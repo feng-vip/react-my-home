3ab2a6620dc5aaad07666fc41fb070d162a6a79e
lijiankang <1418423928@qq.com>
2019-06-23 11:23:29 +0800

    好租客
    my-react-init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css"
+
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// window 修改端口 set PORT=8888 && 
+// mac | linux PORT=8888 &&
+// cross-env 跨平台  npm i cross-env -D
+// cross-env PORT=8888 
+
+// git init\ 初始化git仓库
+// git status
+// git add .
+// git commit -m "初始化提交"
+
+// gitee.com 马云
+
